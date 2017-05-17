47c9a6607896f55e2254b9f63e22c62ca1d72031
Andrea <andrea@wittrock.us>
2017-05-17 12:54:16 -0500

    Create initial config module

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Andrea on 5/17/2017.
+ */
+
+import {o} from "atp-sugar";
+
+import defaults from "config/defaults";
+
+class Config {
+    constructor() {
+        this.defaults = {};
+        this.values = defaults;
+        this.finalValues = defaults;
+    }
+
+    setDefaults(values) {
+        this.defaults = o(this.defaults).merge(values).raw;
+    }
+
+    setValues(values) {
+        this.values = o(this.values).merge(values).raw;
+        this.finalValues = o(this.defaults).merge(this.values).raw;
+    }
+
+    get(path) {
+        return this._get(path, this.finalValues);
+    }
+
+    _get(path, values) {
+        if(path.length === 0) {
+            return values;
+        }
+
+        let pathParts = path.split(".");
+        const node = pathParts.shift();
+
+        if(typeof values[node] === 'undefined') {
+            throw "Missing config value " + node;
+        }
+
+        return this._get(pathParts.join("."), values[node]);
+    }
+}
+
+const config = new Config();
+export default config;
